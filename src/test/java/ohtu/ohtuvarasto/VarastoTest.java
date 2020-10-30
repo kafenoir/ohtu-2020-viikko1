4ea89373f9f7f183cc4a928fd10be88db9eb52ae
Antti_Ojala <antti.i.ojala@helsinki.fi>
2020-10-30 21:21:46 +0200

    Varasto-luokan 100% rivikattavuus

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -13,11 +13,13 @@ import static org.junit.Assert.*;
 public class VarastoTest {
 
     Varasto varasto;
+    Varasto toinenVarasto;
     double vertailuTarkkuus = 0.0001;
 
     @Before
     public void setUp() {
         varasto = new Varasto(10);
+        toinenVarasto = new Varasto(10, 5);
     }
 
     @Test
@@ -63,6 +65,103 @@ public class VarastoTest {
 
         // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void eiVoiLisataNegatiivista() {
+        
+        varasto.lisaaVarastoon(5);
+        
+        varasto.lisaaVarastoon(-1);
+        
+        assertEquals(5, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void liikaaLisattaessaSaldoEiYlitaTilavuutta() {
+        varasto.lisaaVarastoon(11);
+        
+        assertEquals(10, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void eiVoiOttaaNegatiivista() {
+        
+        varasto.lisaaVarastoon(5);
+        
+        varasto.otaVarastosta(-1);
+        
+        assertEquals(5, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void varastostaEiVoiOttaaEnemmanKuinSaldon() {
+        varasto.lisaaVarastoon(5);
+        
+        varasto.otaVarastosta(6);
+        
+        assertEquals(0, varasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test 
+    public void tilavuusNollaJosAnnettuNollaTaiNegatiivinen() {
+        
+        Varasto nollavarasto = new Varasto(0);
+        Varasto negavarasto = new Varasto(-1);
+        
+        assertEquals(0, nollavarasto.getTilavuus(), vertailuTarkkuus);
+        assertEquals(0, negavarasto.getTilavuus(), vertailuTarkkuus);
+    }
+    
+    @Test 
+    public void toinenKonstruktoriLuoVarastonOikeallaTilavuudella() {
+        
+        assertEquals(10, toinenVarasto.getTilavuus(), vertailuTarkkuus);
+ 
+    }
+    
+    @Test 
+    public void toinenKonstruktoriLuoVarastonOikeallaSaldolla() {
+        
+        assertEquals(5, toinenVarasto.getSaldo(), vertailuTarkkuus);
+ 
+    }
+    
+    @Test 
+    public void toinenKonstruktoriTilavuusNollaJosNollaTaiNegatiivinen() {
+        
+        Varasto negavarasto = new Varasto(-1, 0);
+        
+        assertEquals(0, negavarasto.getTilavuus(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void negatiivinenNollaaAlkuSaldon() {
+        
+        Varasto negaSaldoVarasto = new Varasto(10, -1);
+        
+        assertEquals(0, negaSaldoVarasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void tilavuusEiYlityJosLiikaaAlkusaldoa() {
+        
+        Varasto yliSaldoVarasto = new Varasto(10, 11);
+        
+        assertEquals(10, yliSaldoVarasto.getSaldo(), vertailuTarkkuus);
+    }
+    
+    @Test
+    public void varastonMerkkijonoesitysOikein() {
+        
+        String merkkijono = varasto.toString();
+        String toinenMerkkijono = toinenVarasto.toString();
+        
+        String vertailu = "saldo = 0.0, vielä tilaa 10.0";
+        String toinenVertailu = "saldo = 5.0, vielä tilaa 5.0";
+        
+        assertEquals(vertailu, merkkijono);
+        assertEquals(toinenVertailu, toinenMerkkijono);
     }
 
 }
